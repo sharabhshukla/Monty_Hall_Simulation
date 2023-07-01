49a27e950d6895fce3a8c2926378c7f0d09e78c0
Sharabh Shukla <sharabh2019@gmail.com>
2023-07-01 23:27:34 +0530

    fixes for calculating winning chances for different strategies

--- main_sim.py
+++ main_sim.py
@@ -3,13 +3,14 @@ from loguru import logger
 from src.sim.monty_hall_sim import Strategy, Door, MHSim
 
 n_trials = 1_000_000
-outcomes = []
+
 
 logger.info("Running monty hall simulation!!")
 for strategy in Strategy:
+    outcomes = []
     logger.info(f"Running simulation for {strategy}")
     for n in range(n_trials):
-        mh_sim = MHSim(prize_door=choice(list(Door)), strategy=Strategy.FLIP)
+        mh_sim = MHSim(prize_door=choice(list(Door)), strategy=strategy)
         outcomes.append(mh_sim.simulate())
     probability = sum(outcomes)/len(outcomes)
     logger.info(f"probability of winning with {strategy} -> {probability}")
